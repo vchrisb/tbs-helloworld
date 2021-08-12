f591fc109d29fa253eaebb17a121d29ccc62099e
vchrisb <vchrisb@users.noreply.github.com>
2021-08-12 10:46:07 +0200

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app
+web: gunicorn --bind 0.0.0.0:8080 hello:app
--- hello.py
+++ hello.py
@@ -6,15 +6,9 @@ app = Flask(__name__)
 
 COLOR = "#33CC33"
 
-# get CF environment variables
-port = int(os.getenv("PORT", "8080"))
-
 content = '<h1>Hello World!</h1></br>I am a python builpack application'
 content = Markup(content)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html",bgcolor=COLOR,content=content)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
